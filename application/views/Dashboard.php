e3b2c6827ce9abe0fde7f404ad4e8b15d4b43695
Syafiq Asshidqi <75901459+asshdqi05@users.noreply.github.com>
2023-08-19 16:24:48 +0700

    Update Dashboard.php

--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -2,7 +2,7 @@
     <div class="card-header">
         <center>
             <img src="<?= base_url('assets/') ?>dist/img/siatan_logo.png" alt="SI ATAN Logo" width="200">
-            <h1>SI ATAN - Supervisi Keselamatan Pasien</h1>
+            <h1><span class="badge badge-pill badge-light">SI ATAN - Supervisi Keselamatan Pasien</span></h1>
         </center>
     </div>
 </div>
