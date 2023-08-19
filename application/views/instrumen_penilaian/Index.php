0a3345ce86cadd3925b63d0cfd9562b3c76acfac
Syafiq Asshidqi <75901459+asshdqi05@users.noreply.github.com>
2023-08-19 15:46:21 +0700

    update index instrumen penilaian

--- application/views/instrumen_penilaian/Index.php
+++ application/views/instrumen_penilaian/Index.php
@@ -1,78 +1,98 @@
-<div class="row">
+<div class="card card-outline card-lightblue">
+    <div class="card-header">
+        <h3 class="card-title">
+            Instrumen Penilaian Supervisor
+        </h3>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-4 col-6">
+                <!-- small card -->
+                <div class="small-box bg-lightblue">
+                    <div class="inner">
+                        <h5>Instrumen Pengetahuan tentang Supervisi Klinik 4S</h5>
+                        <br>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-list-ol"></i>
+                    </div>
+                    <a href="<?= site_url('Instrumen_penilaian/instrumen_pengetahuan') ?>" class="small-box-footer">
+                        Go <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-6">
+                <!-- small card -->
+                <div class="small-box bg-lightblue">
+                    <div class="inner">
+                        <h5>Instrumen Sikap dalam Supervisi Klinik 4S</h5>
+                        <br>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-list-ol"></i>
+                    </div>
+                    <a href="<?= site_url('Master/ruangan') ?>" class="small-box-footer">
+                        Go <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-6">
+                <!-- small card -->
+                <div class="small-box bg-lightblue">
+                    <div class="inner">
+                        <h5>Instrumen Keterampilan dalam Supervisi Klinis 4S</h5>
+                        <br>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-list-ol"></i>
+                    </div>
+                    <a href="<?= site_url('Master/ruangan') ?>" class="small-box-footer">
+                        Go <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-    <div class="col-lg-4 col-6">
-        <!-- small card -->
-        <div class="small-box bg-lightblue">
-            <div class="inner">
-                <h5>Instrumen Pengetahuan tentang Supervisi Klinik 4S</h5>
-                <br>
-            </div>
-            <div class="icon">
-                <i class="fas fa-list-ol"></i>
-            </div>
-            <a href="<?= site_url('Instrumen_penilaian/instrumen_pengetahuan') ?>" class="small-box-footer">
-                Go <i class="fas fa-arrow-circle-right"></i>
-            </a>
-        </div>
+<div class="card card-outline card-lightblue">
+    <div class="card-header">
+        <h3 class="card-title">
+            Instrumen Penilaian Perawat
+        </h3>
     </div>
-    <div class="col-lg-4 col-6">
-        <!-- small card -->
-        <div class="small-box bg-lightblue">
-            <div class="inner">
-                <h5>Instrumen Sikap dalam Supervisi Klinik 4S</h5>
-                <br>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-lg-4 col-6">
+                <!-- small card -->
+                <div class="small-box bg-lightblue">
+                    <div class="inner">
+                        <h5>Kategori Instrumen Sasaran Keselamatan Pasien</h5>
+                        <br>
+                    </div>
+                    <div class="icon">
+                        <i class="far fa-list-alt"></i>
+                    </div>
+                    <a href="<?= site_url('Instrumen_penilaian/kategori_instrumen_skp') ?>" class="small-box-footer">
+                        Go <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
             </div>
-            <div class="icon">
-                <i class="fas fa-list-ol"></i>
+            <div class="col-lg-4 col-6">
+                <!-- small card -->
+                <div class="small-box bg-lightblue">
+                    <div class="inner">
+                        <h5>Instrumen Supervisi Sasaran Keselamatan Pasien </h5>
+                        <br>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-hospital-alt"></i>
+                    </div>
+                    <a href="<?= site_url('Instrumen_penilaian/instrumen_skp') ?>" class="small-box-footer">
+                        Go <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
             </div>
-            <a href="<?= site_url('Master/ruangan') ?>" class="small-box-footer">
-                Go <i class="fas fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-4 col-6">
-        <!-- small card -->
-        <div class="small-box bg-lightblue">
-            <div class="inner">
-                <h5>Instrumen Keterampilan dalam Supervisi Klinis 4S</h5>
-                <br>
-            </div>
-            <div class="icon">
-                <i class="fas fa-list-ol"></i>
-            </div>
-            <a href="<?= site_url('Master/ruangan') ?>" class="small-box-footer">
-                Go <i class="fas fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-4 col-6">
-        <!-- small card -->
-        <div class="small-box bg-lightblue">
-            <div class="inner">
-                <h5>Kategori Instrumen Sasaran Keselamatan Pasien</h5>
-                <br>
-            </div>
-            <div class="icon">
-                <i class="far fa-list-alt"></i>
-            </div>
-            <a href="<?= site_url('Instrumen_penilaian/kategori_instrumen_skp') ?>" class="small-box-footer">
-                Go <i class="fas fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-4 col-6">
-        <!-- small card -->
-        <div class="small-box bg-lightblue">
-            <div class="inner">
-                <h5>Instrumen Supervisi Sasaran Keselamatan Pasien </h5>
-                <br>
-            </div>
-            <div class="icon">
-                <i class="fas fa-hospital-alt"></i>
-            </div>
-            <a href="<?= site_url('Instrumen_penilaian/instrumen_skp') ?>" class="small-box-footer">
-                Go <i class="fas fa-arrow-circle-right"></i>
-            </a>
         </div>
     </div>
 </div>
